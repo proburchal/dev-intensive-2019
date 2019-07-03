63873ce3bc2e5672ae2dbc9f7caa906520f4809c
Kozlov Kirill <kozlov@sdusi.ru>
2019-07-03 18:42:02 +0300

    add String.stripHtml function and the functions it comprises of
    Added functions:
    - String.removeDuplicates()
    - String.removeTags()
    - String.removeHtmlSpecialChars()
    - String.stripHtml()

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -10,3 +10,21 @@ fun String.truncate(length: Int = 16): String {
         --lastIndex
     return this.substring(0, lastIndex+1) + "..."
 }
+
+fun String.removeDuplicateSpaces(): String =
+    Regex(" ++").replace(this, " ")
+
+fun String.removeTags(): String =
+    Regex("<.*?>").replace(this, "")
+
+fun String.removeHtmlSpecialChars(): String =
+    this.replace("&amp;", "&")
+        .replace("&lt;", "<")
+        .replace("&gt;", ">")
+        .replace("&quot;", "\"")
+        .replace("&apos;", "'")
+
+fun String.stripHtml(): String =
+    this.removeTags()
+        .removeHtmlSpecialChars()
+        .removeDuplicateSpaces()
