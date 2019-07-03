41ac853d8979676023dd8fc65f3494bcf3b74607
Kozlov Kirill <kozlov@sdusi.ru>
2019-07-03 18:40:31 +0300

    add String.truncate function

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,1 +1,12 @@
 package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(length: Int = 16): String {
+    val result = this.trimEnd()
+    if (length >= result.length)
+        return result
+
+    var lastIndex = length-1
+    while ((lastIndex >= 0) && (result[lastIndex] == ' '))
+        --lastIndex
+    return this.substring(0, lastIndex+1) + "..."
+}
