69453806505f59be3162f9afd259b1f075556045
Kozlov Kirill <kozlov@sdusi.ru>
2019-07-12 02:21:36 +0300

    add Activity.hideKeyboard method
    There is no documented solution for this problem.
    The committed solution is based on massively liked comment on stackoverflow:
    https://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard/17789187#17789187

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,1 +1,20 @@
 package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.hideKeyboard() {
+    //Based on https://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard/17789187#17789187
+
+    val imm = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+
+    //Find the currently focused view, so we can grab the correct window token from it.
+    var view = this.currentFocus
+
+    //If no view currently has focus, create a new one, just so we can grab a window token from it
+    if (view == null) {
+        view = View(this)
+    }
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
